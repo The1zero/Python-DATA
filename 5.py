73d5347cca47c6ef8adb3c854a5a2ccbc65c4028
The1zero <chandos562@gmail.com>
2026-09-11 22:50:15 -0600

    Datos

--- 5.py
+++ 5.py
@@ -1,4 +1,7 @@
 import pandas as pd 
+
 df = pd.read_csv('Estudiantes.csv')
 print(df)
 
+convertir = pd.read_excel('Estudiantes.xlsx')
+convertir.to_csv('EstudiantesConvertido.csv', index=None, header=True)
