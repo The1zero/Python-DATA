45ea9694ba11e823a8731c2ca951b3e2bbd93c75
The1zero <chandos562@gmail.com>
2026-09-21 20:28:36 -0600

    Agregando lo faltante

--- 6.py
+++ 6.py
@@ -6,3 +6,31 @@ print(df)
 print(df.iloc[1,3])
 
 print(df.iloc[2,:3])
+
+print(df.loc[1,'Carrera'])
+
+print(df.loc[:3,('Carrera','Nombre')])
+
+print(df.iloc[1, :3])
+
+df2 = df.copy()
+df['TURNO'] = pd.Series(['tarde','noche','tarde','noche','noche','tarde'])
+
+print(df)
+
+Semestre = df.pop('Semestre')
+
+print(df)
+
+df=df.append(pd.Series(['Carlos',27,'M','Sociologia','tarde'],
+index=['Nombre','Edad','Genero','Carrera','TURNO']),ignore_index=True)
+
+print(df)
+
+print(df.drop([1,2]))
+
+print(df[(df['Genero']=='F') & (df['Edad'] >= 22)])
+
+print(df.sort_values('Carrera'))
+print(df.sort_values('Edad'))
+print(df.dropna())
